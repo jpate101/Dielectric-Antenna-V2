54baad14230f8dfd0a6bed5eeaa4bfe4e8aff2c8
jpate101 <62268987+jpate101@users.noreply.github.com>
2025-01-20 10:56:47 +1000

    Version of Code that worked at MT Weld

--- Megiq VNA Server/Python Server/mudmasterui/MudMasterUI/config.py
+++ Megiq VNA Server/Python Server/mudmasterui/MudMasterUI/config.py
@@ -120,7 +120,7 @@ class Config(object):
             },
         },
         'teltonika':{
-            'DEVICE_IP' : '192.168.1.1',
+            'DEVICE_IP' : '192.168.10.1',
             'username' : 'admin',
             'password' : 'MudM45t3r'
             
